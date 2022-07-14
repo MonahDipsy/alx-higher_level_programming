50509e935518bd99b1ff00c122fb176291874eb7
Monicah <98586425+MonahDipsy@users.noreply.github.com>
2022-07-14 12:35:56 +0300

    Task:3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with optional size.
-    Public instance method: def area(self).
-    """
+""" creates class Square """
 
+
+class Square:
+    """ Square class"""
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
 
     def area(self):
-        """Returns the current square area."""
-        return self.__size ** 2
+        return self.__size * self.__size
