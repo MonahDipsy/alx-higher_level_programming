306eade083367613593b2628932d10e41b5137fa
Monicah <98586425+MonahDipsy@users.noreply.github.com>
2022-07-14 12:04:58 +0300

    Task:0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" This module '0-square' defines a class 'Square' """
 class Square:
-    """Represents an empty square."""
+	"""This is a class representation of a square object
+	with empty attriibutes.
+	"""
     pass
